ed3268247ffd344f84b98ce2cdbd909b32c4ba4a
sara1ahmed <sara.ah@trufla.com>
2024-02-12 01:58:45 +0200

    sara section form
    without toggles

--- cypress/Pages/app-form-page.cy.js
+++ cypress/Pages/app-form-page.cy.js
@@ -1,4 +1,107 @@
 //Add Your Code Here
 class AppFormPage{
 
+    get scrollSaraSec(){
+        
+        return cy.get('[path="applicant.shared.city"] > .tru-form-item');
+    } 
+     get BusinessType(){
+        return cy.get('span.ant-select-selection-placeholder').contains('Business Type');
+    }
+     get SelectBusinessType(){
+            return cy.get('.ant-select-item-option-content').contains('Individual');      
+    }
+    get IndustryType(){
+         return cy.get('[path="applicant.shared.industryType"] > .tru-form-item > .tru-column > .select-with-filter-container > .tooltip-container > .tru-single-select-arrow-wrapper > .ant-select > .ant-select-selector');
+        //return cy.get('[class="ant-select-selection-search-input"][id="rc_select_5"]');
+    } 
+    get SelectIndustryType(){
+            return cy.get('.ant-select-item-option-content').contains('Administration of Education Programs');      
+    }
+    /*
+    get DatalockIndustry(){
+        return cy.get('span[class="ant-select-selector"]').contains('Business Type');
+    } 
+    get SelectDatalockIndustry(){
+            return cy.get('.ant-select-item-option-content').contains('Agriculture');      
+    }
+    */
+
+    get BusinessDescription(){
+        return cy.get('textarea[class="tru-textarea"][placeholder="Business Description"]');
+    }
+     /*
+     
+    get AdultContent(){
+        return cy.get('input[type="checkbox"][path=coverage.shared.engagedIndustries.adult"]');
+    }
+    get Cannabis(){
+        return cy.get('input[type="checkbox"][path="coverage.shared.engagedIndustries.cannabis"]').eq(0);
+    }
+    get CareProvider(){
+        return cy.get('input[type="checkbox"][path="coverage.shared.engagedIndustries.careProvider"]').eq(0);
+    }
+    get CryptocurrencyOrBlockchain(){
+        return cy.get('input[type="checkbox"][path="coverage.shared.engagedIndustries.cryptoCurrency"]').eq(0);
+    }
+    get DataProcessing(){
+        return cy.get('input[type="checkbox"][path="coverage.shared.engagedIndustries.dataProcessing"]').eq(0);
+    }
+    get DebtCollection(){
+        return cy.get('input[type="checkbox"][path="coverage.shared.engagedIndustries.debtCollection"]').eq(0);
+    }
+    get Gambling(){
+        return cy.get('input[type="checkbox"][path="coverage.shared.engagedIndustries.gambling"]').eq(0);
+    }
+    get PaymentProcessingn(){
+        return cy.get('input[type="checkbox"][path="coverage.shared.engagedIndustries.paymentProcessing"]').eq(0);
+    }
+    get StorageHosting(){
+        return cy.get('input[type="checkbox"][path="coverage.shared.engagedIndustries.storageHosting"]').eq(0);
+    } */
+
+    get NumberOfEmployees    (){
+        return cy.get('input[type="number"][placeholder="Number Of Employees"]');
+    }
+     
+    get NumberOfNonUSEmployees(){
+        return cy.get('input[type="number"][placeholder="Number of Non US Employees"]');
+    }
+    get AnnualRevenueLastFinancialYear (){
+        return cy.get('input[type="text"][placeholder="Annual Revenue for the last complete financial year $"]');
+    }
+    get GrossProfitLastFinancialYear (){
+        return cy.get('input[type="text"][placeholder="Gross Profit for the last complete financial year $"]');
+    } 
+    get NonUSRevenue(){
+        return cy.get('input[type="radio"][name="applicant.shared.globalRevenue"]').eq(0);
+    }
+    get NonUSRevenueAmount(){
+        return cy.get('input.tru-number-input[placeholder="Non US Revenue Percentage (%)"]');
+    }
+    get EEARevenue(){
+        return cy.get('input[type="radio"][name="applicant.datalock.eeaRevenue"]').eq(0);
+    }
+    get HighRiskTerritories(){
+        return cy.get('input[type="radio"][name="applicant.datalock.highRiskTerritories"]').eq(1);
+    }
+    get IncludeTRIA(){
+        return cy.get('input[type="radio"][name="applicant.datalock.includeTria"]').eq(1);
+    } 
+    get ChangeInBusinessActivities(){
+        return cy.get('input[type="radio"][name="applicant.bbr.mergerAcquisition"]').eq(1);
+    } 
+
+    get TypeOfCoverage(){
+        return cy.get('span.ant-select-selection-placeholder').contains('Type of Coverage');
+        //return cy.get('div[class="ant-select-selection-placeholder"]').contains('Type of Coverage');
+     }
+        
+     get SelectCoverage(){
+            return cy.get('.ant-select-item-option-content').contains("ADMITTED");
+            
+        }
+
 }
+
+module.exports = new AppFormPage();
--- cypress/Tests/app-form-test.cy.js
+++ cypress/Tests/app-form-test.cy.js
@@ -1,4 +1,52 @@
 //Add Your Code Here
+import AppFormPage from "../Pages/app-form-page.cy.js"; 
+
 class AppFormTest{
+    Addingclaims(){
+        //cy.scrollTo(0, 500)
+        //cy.get('#rc_select_16')
+
+        AppFormPage.scrollSaraSec.scrollIntoView();
+        cy.wait(5000);
+        AppFormPage.BusinessType.click({force:true});
+        AppFormPage.SelectBusinessType.click();
+        AppFormPage.scrollSaraSec.scrollIntoView();
+        cy.wait(5000);
+
+        //AppFormPage.IndustryType.Select('Administration of Education Programs');
+        AppFormPage.IndustryType.click();
+        cy.wait(5000);
+        AppFormPage.SelectIndustryType.click();
+        cy.wait(5000);
+        AppFormPage.BusinessDescription.type('business');
+        cy.wait(5000);
+       // AppFormPage.AdultContent.check();
+       AppFormPage.NumberOfEmployees.type('3');
+       AppFormPage.NumberOfNonUSEmployees.type('3');
+       AppFormPage.AnnualRevenueLastFinancialYear.type('1000000');
+       AppFormPage.GrossProfitLastFinancialYear.type('1000001');
+       AppFormPage.NonUSRevenue.click();
+       cy.wait(5000);
+       AppFormPage.NonUSRevenueAmount.dblclick().clear();
+       cy.wait(5000);
+       AppFormPage.NonUSRevenueAmount.type('20');
+       AppFormPage.EEARevenue.click();
+       AppFormPage.HighRiskTerritories.click();
+       AppFormPage.IncludeTRIA.click();
+       AppFormPage.ChangeInBusinessActivities.click();
+       cy.wait(5000);
+       AppFormPage.NonUSRevenue.scrollIntoView();
+       cy.wait(5000);
+       AppFormPage.TypeOfCoverage.click({force:true});
+       cy.wait(5000);
+       AppFormPage.SelectCoverage.click();
+
+       
+
+
+    }
+
+
     
 }
+module.exports=new AppFormTest();
