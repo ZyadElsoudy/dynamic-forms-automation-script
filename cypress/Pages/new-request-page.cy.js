5fcc7a78b979418c359bdec6a1c340acb459222e
ZyadElsoudy <zyad.a@trufla.com>
2024-02-13 13:44:17 +0200

    Push

--- cypress/Pages/new-request-page.cy.js
+++ cypress/Pages/new-request-page.cy.js
@@ -1,9 +1,9 @@
 class NewRequestPage{
    get AddBtn(){return cy.get(".tru-button").contains("Add");}
-   get SelectNewRequest(){return cy.get('span[class="ant-select-selection-item"][title="Auto -Commercial"]');}
+   get SelectNewRequest(){return cy.get('span[class="ant-select-selection-item"][title="Auto"]');}
    get RequestOption(){return cy.get('div[class="ant-select-item ant-select-item-option ant-select-item-option-active"][title="New Cyber Security Request - US"]');}
    get selectNewReq(){return cy.get('.open > .animate__animated > :nth-child(1)');}
-   get SelectClient(){return cy.get('.ant-select-selector').contains('Select Client')    }
+   get SelectClient(){return cy.get('.ant-select-selector').contains('Select Client') }
    get searchresult(){return cy.get('.ant-select-item-option-content').contains("zooz");}
    get SelectBranch(){return cy.get('.ant-select-selection-item').contains('Select Branch');}
    get SelectedBranch(){return cy.get('.ant-select-item-option-content').contains('implementation Egypt')}
