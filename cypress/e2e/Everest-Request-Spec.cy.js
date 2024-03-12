e655687374f7c94ebf90b0a91b5b392899aaac81
ahmedemad179 <146641740+ahmedemad179@users.noreply.github.com>
2024-03-12 11:20:33 +0200

    allure

--- cypress/e2e/Everest-Request-Spec.cy.js
+++ cypress/e2e/Everest-Request-Spec.cy.js
@@ -1,10 +1,9 @@
-import { it } from "mocha"
-import Helper from "../Helpers/test-helper.cy.js";
-import EverestRequstTeste from "../Tests/Everest-Request-Test.cy.js";
-  Helper.CustomBeforeEach();
-  
-  it('Add new Everest Request',()=>{
-   
-    EverestRequstTeste.SubmitEverestRequest();
-  
-  }) 
+import { it } from 'mocha';
+import Helper from '../Helpers/test-helper.cy.js';
+import EverestRequstTeste from '../Tests/Everest-Request-Test.cy.js';
+
+Helper.CustomBeforeEach();
+
+it('Add new Everest Request', () => {
+  EverestRequstTeste.SubmitEverestRequest();
+});
